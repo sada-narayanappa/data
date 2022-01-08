a09a479fa1bebad72be5426b62d50eb2f9ea1d06
sada-narayanappa <meyers@geospaces.org>
2022-01-08 16:31:54 -0700

    Commiting

--- stocks/getstocks.py
+++ stocks/getstocks.py
@@ -11,27 +11,12 @@ from colabexts.jcommon import *
 from alpha_vantage.timeseries import TimeSeries
 from alpha_vantage.fundamentaldata import FundamentalData
 import yfinance as yf
+from utils import *
 
-mpl.use('Agg')
 
 API_KEY = None  # Put your API KEY if you need to test data download or just use the data
-#-----------------------------------------------------------------------------------
-'''
-This will read API_KEY from '~/.keys/keys.json'
-'''
-def getkey(key='password'):
-    API_KEY, lines, file =None, None, os.path.expanduser('~/.keys/keys.json')
-    if os.path.exists(file):
-        with open(file, 'r') as f:
-            r = f.read()
-        j = eval(r)
-        return j['AV_API_KEY'], j['NEWSAPI_KEY']
-    else:
-        raise Exception ("Please supply API_KEY")
-
-    return avk, newsk;
-
 API_KEY, NEWS_KEY = getkey()
+
 #-----------------------------------------------------------------------------------
 '''
 This will read required symbols and saves them to data directory
