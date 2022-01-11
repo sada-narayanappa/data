17e2a0cce99eaa4c7168f9757a34d1a2d0a894b5
sada-narayanappa <meyers@geospaces.org>
2022-01-11 07:12:52 -0700

    Commiting

--- stocks/utils.py
+++ stocks/utils.py
@@ -9,6 +9,15 @@ import numpy as np
 #mpl.use('Agg')
 
 API_KEY = None  # Put your API KEY if you need to test data download or just use the data
+
+#-----------------------------------------------------------------------------------
+def readfile(file, ret=None):
+    if not os.path.exists(file):
+        return ret
+    
+    with (open(file, "r")) as f: 
+        conts = f.read()
+    return conts
 #-----------------------------------------------------------------------------------
 '''
 This will read API_KEY from '~/.keys/keys.json'
@@ -55,14 +64,6 @@ def chk_newer(filename1, filename2 ):
     dt2   = datetime.datetime.fromtimestamp(os.path.getmtime(filename2))
         
     return dt1 >= dt2
-#-----------------------------------------------------------------------------------
-def readfile(file, ret=None):
-    if not os.path.exists(file):
-        return ret
-    
-    with (open(file, "r")) as f: 
-        conts = f.read()
-    return conts
 
 # Example:
 #f1='/opt/data/data/stocks/data/aapl/news_raw.json'
